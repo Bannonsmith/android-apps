fe1dc1114ec897fab231a345b0a2a944c65c4780
Bannon smith <Bannonsmith13@gmail.com>
2021-12-05 13:33:17 -0600

    Tying the button together

--- login/app/src/main/java/com/example/login/MainActivity.java
+++ login/app/src/main/java/com/example/login/MainActivity.java
@@ -3,6 +3,12 @@ package com.example.login;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.material.button.MaterialButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,28 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
+
+        TextView username = findViewById(R.id.username);
+        TextView password = findViewById(R.id.password);
+
+        MaterialButton loginBtn = (MaterialButton) findViewById(R.id.button);
+
+        // admin and admin
+
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(username.getText().toString().equals("admin") && password.getText().toString().equals("admin")) {
+                    //correct
+                    Toast.makeText(MainActivity.this, "Login Successfull", Toast.LENGTH_SHORT).show();
+                }else
+                    //incorrect
+            }
+
+
+
+
+
+            }
+        });
 }
