06ee38ed067788fc9d32655b7c43b50b0118593d
Bannon smith <Bannonsmith13@gmail.com>
2021-12-05 22:41:54 -0600

    set the fields on the activity page

---
+++ SQLLoginRegister/app/src/main/java/com/example/sqlloginregister/DBHelper.java
@@ -0,0 +1,4 @@
+package com.example.sqlloginregister;
+
+public class DBHelper {
+}
--- SQLLoginRegister/app/src/main/java/com/example/sqlloginregister/MainActivity.java
+++ SQLLoginRegister/app/src/main/java/com/example/sqlloginregister/MainActivity.java
@@ -3,12 +3,40 @@ package com.example.sqlloginregister;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    EditText username,password,repassword;
+    Button signUpBtn, goToSignInBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        username = findViewById(R.id.username);
+        password = findViewById(R.id.password);
+        repassword = findViewById(R.id.repassword);
+        goToSignInBtn = findViewById(R.id.goToSigInBtn);
+        signUpBtn = findViewById(R.id.signUpBtn);
+
+        signUpBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        goToSignInBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+
     }
 }
