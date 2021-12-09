f389140081aa24bfaf5991c2eb0487d0e72869a4
Bannon smith <Bannonsmith13@gmail.com>
2021-12-09 16:15:41 -0600

    Created our model

---
+++ SQLLite/app/src/main/java/com/example/sqllite/CustomerModel.java
@@ -0,0 +1,79 @@
+package com.example.sqllite;
+
+import java.util.Objects;
+
+public class CustomerModel {
+
+    private int id;
+    private String name;
+    private int age;
+    private boolean isActive;
+
+    public CustomerModel(int id, String name, int age, boolean isActive) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.isActive = isActive;
+    }
+
+    public CustomerModel() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public boolean isActive() {
+        return isActive;
+    }
+
+    public void setActive(boolean active) {
+        isActive = active;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        CustomerModel that = (CustomerModel) o;
+        return id == that.id && age == that.age && isActive == that.isActive && Objects.equals(name, that.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, age, isActive);
+    }
+
+
+    @Override
+    public String toString() {
+        return "CustomerModel{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", age=" + age +
+                ", isActive=" + isActive +
+                '}';
+    }
+
+    
+}
